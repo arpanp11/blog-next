64b54ac8b01cf5b9637d62b810ada1d8ca5b6283
arpanp11 <arpanpatel2810@gmail.com>
2021-07-27 09:51:13 -0400

    add sidebar for categories

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -6,15 +6,23 @@ import Post from '@/components/Post';
 import { POSTS_PER_PAGE } from '@/config/index';
 import Pagination from '@/components/Pagination';
 import { getPosts } from '@/lib/posts';
+import CategoryList from '@/components/CategoryList';
 
-export default function BlogPage({ posts, numPages, currentPage }) {
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
 
       <Pagination currentPage={currentPage} numPages={numPages} />
@@ -47,6 +55,10 @@ export async function getStaticProps({ params }) {
   const files = fs.readdirSync(path.join('posts'));
   const posts = getPosts();
 
+  // get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category);
+  const uniqueCategories = [...new Set(categories)];
+
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
   const pageIndex = page - 1;
   const orderedPosts = posts.slice(
@@ -59,6 +71,7 @@ export async function getStaticProps({ params }) {
       posts: orderedPosts,
       numPages,
       currentPage: page,
+      categories: uniqueCategories,
     },
   };
 }
