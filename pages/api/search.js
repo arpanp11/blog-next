5dac278a74261104c63eb60af379c1192a69e949
arpanp11 <arpanpatel2810@gmail.com>
2021-07-27 12:15:59 -0400

    fixed typo

--- pages/api/search.js
+++ pages/api/search.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_ENV === 'production') {
-    // fetch from cache -- (cache/data directory)
+    // fetch from cache -- (cache/data.js directory)
     posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
